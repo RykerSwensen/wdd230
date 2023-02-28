30565ee27d950ce4416ae12cec3a373903a495a1
Ryker Swensen <83606669+RykerSwensen@users.noreply.github.com>
2023-02-28 13:08:43 -0700

    changes from week 5 applied to week 6.

--- lesson6/scripts/main.js
+++ lesson6/scripts/main.js
@@ -1,3 +1,5 @@
+// Show the meet-greet message on Tue/Thu
+// Date.getDay() uses a 0 based index to return the day of the week
 var messagedate = new Date();
 if (messagedate.getDay() == 2 || messagedate.getDay() == 4) {
   document.querySelector("#meet-greet").classList.add("active");
